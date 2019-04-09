064d462a83ef5636181cfa4c52e277270e6ee7bb
74716 <hujirui@tospur.com>
2019-04-09 17:32:30 +0800

    demo

--- src/test/java/com/exampl/estemplate/springdateelasticsearch/SpringdateelasticsearchApplicationTests.java
+++ src/test/java/com/exampl/estemplate/springdateelasticsearch/SpringdateelasticsearchApplicationTests.java
@@ -1,11 +1,11 @@
 package com.exampl.estemplate.springdateelasticsearch;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.exampl.estemplate.springdateelasticsearch.repository.BuildingMapper;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.bulk.*;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
@@ -19,11 +19,17 @@ import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.script.Script;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,6 +46,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
@@ -53,6 +60,7 @@ public class SpringdateelasticsearchApplicationTests {
 
     @Autowired
     private BuildingMapper buildingMapper;
+
     @Before
     public void initClent() throws UnknownHostException {
         Settings settings = Settings.builder()
@@ -106,16 +114,71 @@ public class SpringdateelasticsearchApplicationTests {
         System.out.println(">>>>nbHits=" + nbHits);
     }
 
+
+    @Test
+    public void craeteChinaIndexTest() {
+        CreateIndexRequestBuilder builder = client.admin().indices().prepareCreate("china");
+        XContentBuilder mapping = null;
+        try {
+            mapping = XContentFactory.jsonBuilder()
+                    .startObject()
+                    .startObject("properties") //设置之定义字段
+
+                    .startObject("cityName")
+                    .field("type", "text")
+                    //.field("analyzer", "ik_max_word") //设置ik 分词器
+                    .endObject()
+
+                    .startObject("message")
+                    .field("type", "text")
+                   // .field("analyzer", "ik_max_word")
+                    .endObject()
+                    .startObject("years")
+                    .field("type", "date")  //设置Date类型
+                    .field("format", "yyyy-MM-dd HH:mm:ss") //设置Date的格式
+                    .endObject()
+
+                    .endObject()
+                    .endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        builder.addMapping("city", mapping);
+        CreateIndexResponse response = builder.execute().actionGet();
+        System.out.println("tospur create>>>" + response);
+    }
+
     //设置index
     @Test
-    public void setESTest() {
+    public void setChinaIndexTest() {
+
         Map<String, Object> json = new HashMap<>();
-        json.put("cityName", "上海");
-        json.put("years", new Date());
-        json.put("message", "上海市市一个繁华的城市");
-        IndexResponse response = client.prepareIndex("city", "sh").setSource(json).get();
-        System.out.println("in>>>>" + response.status());
+        json.put("cityName", "北京");
+        json.put("years", DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
+        json.put("message", "北京市是一个雾霾的城市");
+        Map<String, Object> json2 = new HashMap<>();
+        json2.put("cityName", "上海");
+        json2.put("years", DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
+        json2.put("message", "上海市一个繁华的城市");
+        IndexResponse response1 = client.prepareIndex("china", "city").setSource(json).get();
+        IndexResponse response2 = client.prepareIndex("china", "city").setSource(json2).get();
+        //System.out.println("in>>>>" + response.status());
     }
+
+    //prepareSearch 检索数据
+    @Test
+    public void getChinaIndexTest() throws ExecutionException, InterruptedException {
+        SearchResponse searchResponse= client
+                .prepareSearch("china").setQuery(QueryBuilders.matchQuery("message","繁")).get(); //.search(searchRequest).get();
+        SearchHit[] searchHits= searchResponse.getHits().getHits();
+        JSONArray jsonArray=new JSONArray();
+        for (SearchHit hit: searchHits) {
+            jsonArray.add(JSONObject.parse(hit.getSourceAsString()));
+        }
+        String json=jsonArray.toJSONString();
+        System.out.println(">>>>"+json);
+    }
+
 
     //通过脚本更新操作
     @Test
@@ -151,51 +214,51 @@ public class SpringdateelasticsearchApplicationTests {
     public void createIndexTest() throws IOException {
         CreateIndexRequestBuilder builder = client.admin().indices().prepareCreate("tospurweb");
         XContentBuilder mapping = XContentFactory.jsonBuilder()
-                 .startObject()
+                .startObject()
                 .startObject("properties") //设置之定义字段
 
                 .startObject("id")
-                .field("type","integer") //设置数据类型
+                .field("type", "integer") //设置数据类型
                 .endObject()
 
                 .startObject("building_id")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("dataln_building_id")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("dataln_building_unit_id")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("dataln_presale_id")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("unit_name")
-                .field("type","text")
+                .field("type", "text")
                 .endObject()
 
                 .startObject("layers")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("sets")
-                .field("type","integer")
+                .field("type", "integer")
                 .endObject()
 
                 .startObject("tags")
-                .field("type","text")
+                .field("type", "text")
                 .endObject()
 
                 .startObject("left_margin")
-                .field("type","double")
+                .field("type", "double")
                 .endObject()
 
                 .startObject("top_margin")
-                .field("type","double")
+                .field("type", "double")
                 .endObject()
 
 //                .startObject("unit_name")
@@ -203,19 +266,73 @@ public class SpringdateelasticsearchApplicationTests {
 //                .field("format","yyyy-MM-dd HH:mm:ss") //设置Date的格式
                 .endObject()
                 .endObject();
-        builder.addMapping("building_unit",mapping);
-        CreateIndexResponse response= builder.execute().actionGet();
+        builder.addMapping("building_unit", mapping);
+        CreateIndexResponse response = builder.execute().actionGet();
         System.out.println("tospur create>>>" + response);
     }
 
-    //批量导入数据
+    //批量导入数据,循环每次导入
     @Test
     public void bulkESTest() throws ExecutionException, InterruptedException {
-        List<Map<String,Object>> maps= buildingMapper.findAll();
+        List<Map<String, Object>> maps = buildingMapper.findAll();
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+        for (Map<String, Object> map :
+                maps) {
+            bulkRequest.add(client.prepareIndex("tospurweb", "building_unit")
+                    .setSource(map));
+        }
+        BulkResponse responses = bulkRequest.get();
+        System.out.println("bulk>>>>>" + responses.getIngestTookInMillis());
+    }
 
-        BulkRequestBuilder bulkRequest=client.prepareBulk();
+    @Test
+    public void searchBuildingTest(){
+        SearchResponse searchResponse= client.prepareSearch("tospurweb")
+                .setSize(30)
+                .setFrom(0)//213750
+                .setQuery(QueryBuilders.matchQuery("unit_name","露香园")).get();
+        System.out.println(">>>查询用时："+searchResponse.getTook().getMillis());
+        System.out.println(">>>查询总记录数："+searchResponse.getHits().getTotalHits());
+        SearchHit[]  searchHits= searchResponse.getHits().getHits();
+        JSONArray jsonArray=new JSONArray();
+        for (SearchHit hit: searchHits) {
+            //hit.getScore() 获取匹配分数
+           jsonArray.add(JSONObject.parse(hit.getSourceAsString()));
+        }
+        System.out.println(">>>size="+jsonArray.size()+""+jsonArray.toString());
+    }
+    //批处理
+    @Test
+    public void bulkProcessor() throws InterruptedException {
+        BulkProcessor bulkProcessor = BulkProcessor.builder(
+                client //①添加您的Elasticsearch客户端
+                , new BulkProcessor.Listener() {
+                    //②在批量执行之前调用此方法
+                    @Override
+                    public void beforeBulk(long executionId, BulkRequest request) {
+                    }
 
-       // bulkRequest.add(new IndexRequest("tospurweb","building_unit").)
-       // client.bulk(bulkRequest);
+                    //③批量执行后调用此方法
+                    @Override
+                    public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                    }
+
+                    //④批量失败并引发a时调用此方法 Throwable
+                    @Override
+                    public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                    }
+                })
+                .setBulkActions(10000)//⑤每10000个请求执行批量处理/默认值是1000
+                .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))//⑥每5mb提交一次/默认5mb
+                .setFlushInterval(TimeValue.timeValueSeconds(5))//⑦无论请求数量多少，我们都希望每隔5秒刷新一次/默认无
+                .setConcurrentRequests(1)//⑧设置并发请求数。值为0表示只允许执行单个请求。值1表示允许执行1个并发请求，同时累积新的批量请求/默认1
+                .setBackoffPolicy(
+                        BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))//⑨Set a custom backoff policy which will initially wait for 100ms, increase exponentially and retries up to three times. A retry is attempted whenever one or more bulk item requests have failed with an EsRejectedExecutionException which indicates that there were too little compute resources available for processing the request. To disable backoff, pass BackoffPolicy.noBackoff()/默认值重试次数为8次，启动延迟为50ms。总等待时间约为5.1秒。
+                .build();
+
+        bulkProcessor.add(new IndexRequest("tospurweb", "building_unit").source(new HashMap()));
+        bulkProcessor.flush();//刷新剩余的请求
+        bulkProcessor.awaitClose(10, TimeUnit.MICROSECONDS);//awaitClose方法将等待所有批量请求完成的指定超时true，如果在所有批量请求完成之前经过指定的等待时间， false则返回/close方法不会等待任何剩余的批量请求完成并立即退出
+
     }
 }
